53b6bf120cf68a91ccb1352a87fa999fe23da099
Notshiso <162983188+Notshiso@users.noreply.github.com>
2024-03-11 12:58:52 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -12,3 +12,9 @@ if genre == ':rainbow[foot]':
 else:
     st.write("wallah jte bz.")
 
+input="le meilleur brawler"
+list_possibilities=["spike","léon","corbac","edgar"]
+st.write("choisis ."+ input)
+for i in range(len(list_posibilities)):
+    st.button(list_possibilities[i])
+    
